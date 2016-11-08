1bb40f84dc46b52ef618ea8c2029d758037b9327
stevenhurwitt <stevenhurwitt@gmail.com>
2016-11-08 15:41:13 -0600

    Update var_labels.R

--- bin/var_labels.R
+++ bin/var_labels.R
@@ -3,8 +3,8 @@ setwd("/your/wd/here")
 afternoon = read.csv("afternoonrates.csv", header = T, sep = ",")
 evening = read.csv("eveningrates.csv", header = T, sep = ",")
 
-vars = c("mode", "guard", '#', 'best rate', 'name', 'idx', 'airtime',
-         'max_tp', 'avg(tp)', 'avg(prob)', 'sd(prob)', 'prob',
+vars = c("mode", "guard", '#', 'best_rate', 'name', 'idx', 'airtime',
+         'max_tp', 'avg_tp', 'avg_prob', 'sd_prob', 'prob',
          'retry', 'suc', 'att', 'sum_suc', 'sum_att')
 
 colnames(afternoon) = vars
